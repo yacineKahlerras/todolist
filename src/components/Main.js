afc8fbda94cfdaca362aedaeb893182164ed9a8e
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-11-03 22:07:33 +0100

    ok

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,10 +13,24 @@ export default function Main() {
     },
   ]);
 
+  // gets data from local storage
+  React.useEffect(() => {
+    const storageTasks = JSON.parse(localStorage.getItem("tasks"));
+    console.log(storageTasks);
+    if (storageTasks.length > 0) setTasks(storageTasks);
+  }, []);
+
+  // update local storage everytime a value change
+  React.useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  // delete a task from list
   function deleteTask(id) {
-    setTasks((previousTasks) => previousTasks.filter((task) => task.id != id));
+    setTasks((previousTasks) => previousTasks.filter((task) => task.id !== id));
   }
 
+  // gets task from array and reverses its "isDone" boolean
   function checkTask(id) {
     setTasks((previousTasks) =>
       previousTasks.map((t) => ({
@@ -26,8 +40,22 @@ export default function Main() {
     );
   }
 
+  // adds new task to first position of tasks array
+  function createNewTask(event) {
+    if (event.key !== "Enter") return;
+    const value = event.target.value.trim();
+    event.target.value = "";
+    const newTask = {
+      id: nanoid(),
+      todoText: value,
+      isDone: false,
+    };
+    setTasks([newTask, ...tasks]);
+  }
+
   return (
     <main>
+      <input placeholder="create a new todo" onKeyPress={createNewTask}></input>
       <TaskList tasks={tasks} checkTask={checkTask} deleteTask={deleteTask} />
     </main>
   );
