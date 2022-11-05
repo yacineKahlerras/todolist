93cb938e92bf7c459162c51ce7aea75d30496dcf
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-11-05 14:09:07 +0100

    ok

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Header from "./Header";
 export default function App() {
   return (
     <div className="container">
+      <div className="bg-cover"></div>
       <Header />
       <Main />
     </div>
