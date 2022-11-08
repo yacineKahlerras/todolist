4dda27ceb1ebf868834cf4daf00153e149b0df56
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-11-08 11:32:23 +0100

    ok

--- src/Child.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Chichi from "./chichi";
-
-export default function Child() {
-  return <Chichi />;
-}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./components/App";
-import MyApp from "./MyApp";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<App />);
-root.render(<MyApp />);
+root.render(<App />);
